e6ae2c8ac8018d181c4d4f9c35fb17db913522fe
wjfatuan <32616077+wjfatuan@users.noreply.github.com>
2019-11-10 09:05:11 -0500

    Adding routing

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+
+class Home extends React.Component {
+  render() {
+      return <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            </div>
+            ;
+  }
+}
+
+export default Home;
---
+++ src/Page1.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Page1 extends React.Component {
+  render() {
+      return <div>Hello</div>;
+  }
+}
+
+export default Page1;
